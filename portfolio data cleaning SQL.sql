b6bb440a81dc21cd2f57ba4d8d5de45c1d2a5978
Melat-arch <143753765+Melat-arch@users.noreply.github.com>
2024-08-07 02:11:53 -0500

    Add files via upload

---
+++ portfolio data cleaning SQL.sql
@@ -0,0 +1,182 @@
+--Data cleaning in SQL
+--Data source: Nashville Housing Data 
+
+Select * from [PortfolioProject].dbo.[NashvilleHousing]
+
+Select saledate from [PortfolioProject].dbo.[NashvilleHousing]
+--Changing datatype
+ Alter table dbo.[NashvilleHousing]
+ alter column saledate date
+
+ Select PropertyAddress
+ from [PortfolioProject].dbo.[NashvilleHousing]
+ where PropertyAddress is null
+
+ --we have 29 rows that are null
+
+ --populate property address data
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress , ISNULL(a.PropertyAddress, b.PropertyAddress)
+from [PortfolioProject].dbo.[NashvilleHousing] a
+inner join [PortfolioProject].dbo.[NashvilleHousing] b
+on a.ParcelID = b.ParcelID 
+and a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
+
+update a
+set
+PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+from [PortfolioProject].dbo.[NashvilleHousing] a
+inner join [PortfolioProject].dbo.[NashvilleHousing] b
+on a.ParcelID = b.ParcelID 
+and a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
+
+
+--breaking out address into individual columns(address, city, state)
+
+
+Select PropertyAddress
+ from [PortfolioProject].dbo.[NashvilleHousing]
+ --where PropertyAddress is null
+
+
+  select SUBSTRING(PropertyAddress,1, CHARINDEX(',', PropertyAddress)-1) as address,
+  SUBSTRING(PropertyAddress,CHARINDEX(',', PropertyAddress)+1, CHARINDEX(',', PropertyAddress))
+  from [PortfolioProject].dbo.[NashvilleHousing]
+  --or
+  select SUBSTRING(PropertyAddress,1, CHARINDEX(',', PropertyAddress)-1) as address,
+  SUBSTRING(PropertyAddress,CHARINDEX(',', PropertyAddress)+1,len (PropertyAddress))
+  from [PortfolioProject].dbo.[NashvilleHousing]
+  --add new column
+Alter table dbo.[NashvilleHousing]
+Add propertySplitAddress nvarchar(255);
+  --populate column
+update dbo.[NashvilleHousing]
+Set propertySplitAddress = SUBSTRING(PropertyAddress,1, CHARINDEX(',', PropertyAddress)-1)
+--add new column
+Alter table dbo.[NashvilleHousing]
+Add propertySplitcity nvarchar(255);
+  --populate column
+update dbo.[NashvilleHousing]
+set propertySplitcity = SUBSTRING(PropertyAddress,CHARINDEX(',', PropertyAddress)+1,len (PropertyAddress))
+
+--Select * from dbo.[NashvilleHousing]
+
+--add new column
+Alter table dbo.[NashvilleHousing]
+Add ownerSplitAddress nvarchar(255);
+
+update dbo.[NashvilleHousing]
+set ownerSplitAddress =  PARSEname(replace(owneraddress, ',', '.'),3)
+
+Alter table dbo.[NashvilleHousing]
+Add ownerSplitcity nvarchar(255);
+
+update dbo.[NashvilleHousing]
+set ownerSplitcity = PARSEname(replace(owneraddress, ',', '.'),2)
+
+
+Alter table dbo.[NashvilleHousing]
+Add ownerSplitstate nvarchar(255);
+
+update dbo.[NashvilleHousing]
+set ownerSplitstate = PARSEname(replace(owneraddress, ',', '.'),1)
+
+--Select *from dbo.[NashvilleHousing]
+
+Select distinct (SoldAsVAcant), count(soldasvacant)
+from dbo.[NashvilleHousing]
+group by soldasvacant
+order by 2
+--fixing some values 
+
+update dbo.[NashvilleHousing]
+set soldasvacant =
+case when soldasvacant = 'N' then 'No'
+     when soldasvacant = 'Y' then 'Yes'
+	 else soldasvacant
+	 end
+
+--check duplicates
+ Select [ParcelID],
+[PropertyAddress],
+[SalePrice],
+[SaleDate],
+[LegalReference] , count(*)       
+from dbo.[NashvilleHousing]
+group by [ParcelID],
+[PropertyAddress],
+[SalePrice],
+[SaleDate],
+[LegalReference]
+having  count(*) >1
+
+--remove duplicates
+;with cte as (
+Select *, row_number() over (
+          partition by [ParcelID],
+[PropertyAddress],
+[SalePrice],
+[SaleDate],
+[LegalReference]        
+ order by [UniqueID ]) as rnk
+from dbo.[NashvilleHousing]
+--order by [ParcelID]
+)
+ delete from cte
+ where rnk >1
+
+--delete unused columns
+Select * from [PortfolioProject].dbo.[NashvilleHousing]
+
+alter table [PortfolioProject].dbo.[NashvilleHousing]
+drop column owneraddress, taxdistrict, propertyAddress
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
